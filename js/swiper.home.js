4348575d394ddf60edafc8a860c033bd5f13fe16
notvo <128831732+notvo@users.noreply.github.com>
2023-07-25 16:26:29 +0500

    Portfolio site

---
+++ js/swiper.home.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
